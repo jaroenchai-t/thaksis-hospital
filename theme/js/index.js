45cb3fcb9af6987ae965a756fd6233726f990ba7
Jaroenchai Tantipong <jaroenchai_t@softsquaregroup.com>
2024-04-04 18:24:24 +0700

    update

--- theme/js/index.js
+++ theme/js/index.js
@@ -5,7 +5,7 @@
  * GITHUB: https://github.com/themefisher/
  */
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.clinic-swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -45,8 +45,66 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+//articles-swiper
+const swiper2 = new Swiper('.articles-swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 15,
+  slidesPerView:4,
+   // Responsive breakpoints
+   breakpoints: {
+    // when window width is >= 0px
+    0: {
+      slidesPerView: 1,
 
-const swiper2 = new Swiper('.swiper-package', {
+    },
+    // when window width is >= 640px
+
+    980: {
+      slidesPerView: 2,
+
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 4,
+
+    },
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true,
+    dynamicMainBullets:1
+  },
+
+
+});
+
+
+const swiper3 = new Swiper('.swiper-package', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 15,
+  slidesPerView:2,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true,
+    dynamicMainBullets:1
+  },
+
+
+});
+
+//
+
+const swiper4 = new Swiper('.swiper-package-mobile', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -59,8 +117,8 @@ const swiper2 = new Swiper('.swiper-package', {
       slidesPerView: 1,
 
     },
-     // when window width is >= 800px
-     800: {
+     // when window width is >= 750px
+     790: {
       slidesPerView: 2,
 
     }
